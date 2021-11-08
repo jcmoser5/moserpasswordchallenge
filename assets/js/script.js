0766ad091093e13817c4994b40773b70f9d1e751
Jonathan Moser <jonathancmoser@gmail.com>
2021-11-07 21:29:25 -0500

    added arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  // create arrays of the acceptable character types
+  
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "%", "!", "'", ",", ".", "/", ":", ";", "<", ">", "?", "[", "]", "`", "{", "}", "|", "~"];
+  
+  // results of user input 
+   
+  var resultArray = [];
+  var userArray = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
